74a8166b704c6a560e3e1a9c0fe5ac31dc25581e
Eliicatss <karenelizabet2828@gmail.com>
2025-03-15 21:59:13 -0500

    ejercicio aleatorio terminado
    Ejercicio de acceso aleatorio a una linea y leer e imprimir su informacion terminado

--- ejercicioAccesoAleatorio.java
+++ ejercicioAccesoAleatorio.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 public class ejercicioAccesoAleatorio {
 
     public static void main(String[] args) {
@@ -10,9 +13,27 @@ public class ejercicioAccesoAleatorio {
                 escritura.write("\n Linea: " + (i+1) + ": Este es el dato " + (i+1));
             }
             escritura.close();
+
+            RandomAccessFile accfile = new RandomAccessFile(archivo, "r");
+            List<String> lineas = new ArrayList<>();
+            String linea;
+            Random random = new Random();
+
+            
+            while ((linea = accfile.readLine()) != null) {
+                lineas.add(linea);
+            }
+
+            if(!lineas.isEmpty()){
+                String randomlinea = lineas.get(random.nextInt(lineas.size()));
+                System.out.println("Linea aleatoria:" + randomlinea );
+            } else {
+                System.out.println("El archivo esta vacio");
+            }
+            accfile.close();
         }
         catch (Exception e) {
-            System.out.println("Error al crear el archivo " + e);
+            System.out.println("Error al crear o leer el archivo " + e);
         }
     }
 }
