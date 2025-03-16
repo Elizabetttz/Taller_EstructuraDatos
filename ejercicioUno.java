20e89d63b2f343531be5dc0d57056027ebdb7308
Dobando-sys <obandodesart@gmail.com>
2025-03-15 20:01:23 -0500

    Fin primer ejercicio
    Todo funcionando

--- ejercicioUno.java
+++ ejercicioUno.java
@@ -1,5 +1,23 @@
+import java.io.*;
+
 public class ejercicioUno {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        try {
+            File archivo = new File("Archivo.txt");
+            FileWriter escritura = new FileWriter(archivo);
+            escritura.write("Linea 1: Este es el primer dato");
+            escritura.write("\n Linea 2: Este es el segundo dato");
+            
+            escritura.close();
+            if(archivo.createNewFile()) {
+                System.out.println("Archivo creado: " + archivo.getName());
+            }
+            else {
+                System.out.println("El archivo ya existe");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error al crear el archivo " + e);
+        }
     }
 }
